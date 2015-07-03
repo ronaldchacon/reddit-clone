9549f8909b5160a6bd454d7cc2e89bff395582f0
Ronald Chacon <ronald.chacon87@gmail.com>
2015-07-03 16:14:50 -0700

    Fix user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,5 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
   EMAIL_FORMAT = /\A([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})\z/
-  validates :email, presence: true, format: {with: EMAIL_FORMAT, message: "Invalid Email"}
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :email, format: {with: EMAIL_FORMAT, message: "Invalid Format"}
 end
