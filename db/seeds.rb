77433cb2c0e3527c8aacaf660485a084ab1a0a43
Ronald Chacon <ronald.chacon87@gmail.com>
2015-10-05 14:48:31 -0700

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-# 100.times do
-#   User.create(
-#     email: Faker::Internet.email, password: 'foobar',
-#     password_confirmation: 'foobar', username: Faker::Internet.user_name
-#   )
-# end
+100.times do
+  User.create(
+    email: Faker::Internet.email, password: 'foobar',
+    password_confirmation: 'foobar', username: Faker::Internet.user_name
+  )
+end
 
-# User.all.each do |u|
-#   rand(1..10).times do
-#     u.posts.create(
-#       title: Faker::Hacker.say_something_smart, url: Faker::Internet.url
-#     )
-#   end
-# end
+User.all.each do |u|
+  rand(1..10).times do
+    u.posts.create(
+      title: Faker::Hacker.say_something_smart, url: Faker::Internet.url
+    )
+  end
+end
 
-# Post.all.each do |post|
-#   rand(1..10).times do
-#     post.comments.create(
-#       user_id: User.all.sample.id, body: Faker::Hacker.say_something_smart
-#     )
-#   end
-# end
+Post.all.each do |post|
+  rand(1..10).times do
+    post.comments.create(
+      user_id: User.all.sample.id, body: Faker::Hacker.say_something_smart
+    )
+  end
+end
 
-# Post.all.each do |post|
-#   # upvote
-#   rand(1..20).times do
-#     post.upvote_by User.all.sample
-#   end
-#   # downvote
-#   rand(1..20).times do
-#     post.downvote_by User.all.sample
-#   end
-# end
+Post.all.each do |post|
+  # upvote
+  rand(1..20).times do
+    post.upvote_by User.all.sample
+  end
+  # downvote
+  rand(1..20).times do
+    post.downvote_by User.all.sample
+  end
+end
 
 Comment.all.each do |comment|
   # upvote
